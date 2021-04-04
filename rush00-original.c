58b8810e61d4829f1bb687a3dda99ee7c1663343
clobato- <clobato-@student.42sp.org.br>
2021-04-04 14:26:20 +0000

    A

--- rush00-original.c
+++ rush00-original.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush00-teste.c                                     :+:      :+:    :+:   */
+/*   rush00.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: clobato- <clobato-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 20:22:17 by vida-sil          #+#    #+#             */
-/*   Updated: 2021/04/04 14:25:12 by clobato-         ###   ########.fr       */
+/*   Updated: 2021/04/04 14:08:58 by clobato-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,32 @@ void	ft_putchar(char a);
 
 void	rush(int x, int y)
 {
-	int a;
-	int l;
+	int height;
+	int width;
 
-	a = 0;
-	while ((a < y) && (x > 0))
+	height = 0;
+	while ((height < y) && (x > 0))
 	{
-		l = 0;
-		while (l < x)
+		width = 0;
+		while (width < x)
 		{
-			if ((a == 0) || (a == (y - 1)) && ((l == 0) || (l == (x - 1))))
-				ft_putchar('o');
+			if ((height == 0) || (height == (y - 1)))
+			{
+				if ((width == 0) || (width == (x - 1)))
+					ft_putchar('o');
+				else
+					ft_putchar('-');
+			}
 			else
-				ft_putchar('-');
-			if ((a != 0) && (a != (y - 1)) && ((l == 0) || l == (x - 1)))
-				ft_putchar('|');
-			else
-				ft_putchar(' ');
-			l++;
+			{
+				if ((width == 0) || width == (x - 1))
+					ft_putchar('|');
+				else
+					ft_putchar(' ');
+			}
+			width++;
 		}
 		ft_putchar('\n');
-		a++;
+		height++;
 	}
 }
