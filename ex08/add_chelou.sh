8033645db6198ef5bcf7b0048302fb2ccb50a6d7
ealves-d <ealves-d@student.42sp.org.br>
2021-04-06 15:25:49 +0000

    termineii

--- ex08/add_chelou.sh
+++ ex08/add_chelou.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
- echo $FT_NBR1,$FT_NBR2 | xargs = echo $'ibase=5; $obase=13;' | tr "\'\\\\\"\?\1" "01234" | tr "mrdoc" "01234" | gtaio luSnemf "012345678ABC" 
+echo $FT_NBR1+$FT_NBR2 | tr "\'\\\\\"?!mrdoc" '0123401234' | xargs echo "obase=13; ibase=5;" | bc | tr "0123456789ABC" "gtaio luSnemf"
